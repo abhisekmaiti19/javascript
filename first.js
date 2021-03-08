53e3126d9361a5bf3ce7ff862b26f534e07b0053
abhisekmaiti19 <63227192+abhisekmaiti19@users.noreply.github.com>
2021-03-08 19:15:35 +0530

    getting started

---
+++ first.js
@@ -0,0 +1,9 @@
+//brief introduction of javascript
+//part 1
+// what is javascript = javascript is high-level object-oriented, multi-paradigm programming lanaguage
+//what is highlevel language = we don't have to eorry about complex stuff like memory management
+
+// wht is the role in js in web development
+// to make dynamic and interective in web pages
+// for example html is noun, css = adjective, js = verb(behaiviour)
+// emascript 6 is biggest update to the language ever
